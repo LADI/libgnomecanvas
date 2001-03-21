7977dd1385989086348bcb7b8435cf083b109f75
Martin Baulig <martin@src.gnome.org>
2001-03-21 11:49:11 +0000

    Initial revision

---
+++ libgnomecanvas/gnomecanvastypes.c
@@ -0,0 +1,47 @@
+#include <config.h>
+#include <gtk/gtktypeutils.h>
+#include "gnome-canvas.h"
+#include "gnome-canvas-util.h"
+
+#include "gnomecanvastypebuiltins.h"
+#include "gnomecanvastypebuiltins_vars.c"
+#include "gnomecanvastypebuiltins_evals.c"
+void gnome_canvas_type_init(void);
+
+void
+gnome_canvas_type_init(void) {
+  static gboolean initialized = FALSE;
+
+  if (!initialized) {
+    int i;
+
+    static struct {
+      gchar           *type_name;
+      GtkType         *type_id;
+      GtkType          parent;
+      gconstpointer    pointer1;
+      gpointer         pointer2;
+    } builtin_info[GNOME_TYPE_NUM_BUILTINS + 1] = {
+#include "gnomecanvastypebuiltins_ids.c"
+      { NULL }
+    };
+
+    initialized = TRUE;
+
+    for (i = 0; i < GNOME_TYPE_NUM_BUILTINS; i++)
+      {
+	GtkType type_id = GTK_TYPE_INVALID;
+	g_assert (builtin_info[i].type_name != NULL);
+	if ( builtin_info[i].parent == GTK_TYPE_ENUM )
+	  type_id = g_enum_register_static (builtin_info[i].type_name, (GtkEnumValue *)builtin_info[i].pointer1);
+	else if ( builtin_info[i].parent == GTK_TYPE_FLAGS )
+	  type_id = g_flags_register_static (builtin_info[i].type_name, (GtkFlagValue *)builtin_info[i].pointer1);
+	else if ( builtin_info[i].parent == GTK_TYPE_BOXED )
+	  type_id = g_boxed_type_register_static (builtin_info[i].type_name, (GBoxedCopyFunc)builtin_info[i].pointer1, (GBoxedFreeFunc)builtin_info[i].pointer2);
+
+	g_assert (type_id != GTK_TYPE_INVALID);
+	(*builtin_info[i].type_id) = type_id;
+      }
+  }
+}
+
