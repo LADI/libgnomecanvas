c1e8a33ff2f4b547f042f0f5752074be45a28845
Martin Baulig <baulig@suse.de>
2001-04-12 12:13:19 +0000

    New header file. It #includes <libgnomecanvas/gnome-canvas.h> and
    2001-04-12  Martin Baulig  <baulig@suse.de>
    
    	* libgnomecanvas.h: New header file.
    	It #includes <libgnomecanvas/gnome-canvas.h> and
    	<libgnomecanvas/gnome-canvas-util.h> and also contains the
    	module init stuff.
    
    	* libgnomecanvastypes.c (libgnomecanvas_module_info):
    	New global variable; this is the `GnomeModuleInfo' structure
    	to initialize this module.

--- libgnomecanvas/libgnomecanvastypes.c
+++ libgnomecanvas/libgnomecanvastypes.c
@@ -1,50 +1,99 @@
+/*
+ * Copyright (C) 1999, 2000 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This file is part of the Gnome Library.
+ *
+ * The Gnome Library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * The Gnome Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with the Gnome Library; see the file COPYING.LIB.  If not,
+ * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+/*
+  @NOTATION@
+ */
+
 #include <config.h>
 #include <gobject/genums.h>
 #include <gobject/gboxed.h>
-#include "gnome-canvas.h"
-#include "gnome-canvas-util.h"
+
+#include <libgnomecanvas/libgnomecanvas.h>
+#include <libgnome/libgnome.h>
+
+static void gnome_canvas_type_init (void);
+static void libgnomecanvas_pre_args_parse (GnomeProgram *,
+					   const GnomeModuleInfo *);
+
+GnomeModuleInfo libgnomecanvas_module_info;
+
+static GnomeModuleRequirement libgnomecanvas_requirements[] = {
+    {VERSION, &libgnome_module_info},
+    {NULL}
+};
+
+GnomeModuleInfo libgnomecanvas_module_info = {
+    "libgnomecanvas", VERSION, "GNOME Canvas",
+    libgnomecanvas_requirements,
+    libgnomecanvas_pre_args_parse, NULL, NULL,
+    NULL,
+};
+
+static void
+libgnomecanvas_pre_args_parse (GnomeProgram *program,
+			       const GnomeModuleInfo *mod_info)
+{
+    gnome_canvas_type_init ();
+}
 
 #include "libgnomecanvastypebuiltins.h"
 #include "libgnomecanvastypebuiltins_vars.c"
 #include "libgnomecanvastypebuiltins_evals.c"
-void gnome_canvas_type_init(void);
 
-void
-gnome_canvas_type_init(void) {
-  static gboolean initialized = FALSE;
+static void
+gnome_canvas_type_init (void)
+{
+    static gboolean initialized = FALSE;
 
-  if (!initialized) {
-    int i;
+    if (!initialized) {
+	int i;
 
-    static struct {
-      gchar           *type_name;
-      GType           *type_id;
-      GType            parent;
-      gconstpointer    pointer1;
-      gconstpointer    pointer2;
-      gconstpointer    pointer3;
-      gboolean         boolean1;
-    } builtin_info[GNOME_TYPE_NUM_BUILTINS + 1] = {
+	static struct {
+	    gchar           *type_name;
+	    GType           *type_id;
+	    GType            parent;
+	    gconstpointer    pointer1;
+	    gconstpointer    pointer2;
+	    gconstpointer    pointer3;
+	    gboolean         boolean1;
+	} builtin_info [GNOME_TYPE_NUM_BUILTINS + 1] = {
 #include "libgnomecanvastypebuiltins_ids.c"
-      { NULL }
-    };
+	    { NULL }
+	};
 
-    initialized = TRUE;
+	initialized = TRUE;
 
-    for (i = 0; i < GNOME_TYPE_NUM_BUILTINS; i++)
-      {
-	GType type_id = G_TYPE_INVALID;
-	g_assert (builtin_info[i].type_name != NULL);
-	if ( builtin_info[i].parent == G_TYPE_ENUM )
-	  type_id = g_enum_register_static (builtin_info[i].type_name, (GEnumValue *)builtin_info[i].pointer1);
-	else if ( builtin_info[i].parent == G_TYPE_FLAGS )
-	  type_id = g_flags_register_static (builtin_info[i].type_name, (GFlagsValue *)builtin_info[i].pointer1);
-	else if ( builtin_info[i].parent == G_TYPE_BOXED )
-	  type_id = g_boxed_type_register_static (builtin_info[i].type_name, (GBoxedInitFunc)builtin_info[1].pointer1, (GBoxedCopyFunc)builtin_info[i].pointer2, (GBoxedFreeFunc)builtin_info[i].pointer2, builtin_info[i].boolean1);
-
-	g_assert (type_id != G_TYPE_INVALID);
-	(*builtin_info[i].type_id) = type_id;
-      }
-  }
+	for (i = 0; i < GNOME_TYPE_NUM_BUILTINS; i++) {
+	    GType type_id = G_TYPE_INVALID;
+	    g_assert (builtin_info[i].type_name != NULL);
+	    if (builtin_info[i].parent == G_TYPE_ENUM)
+		type_id = g_enum_register_static (builtin_info[i].type_name, (GEnumValue *)builtin_info[i].pointer1);
+	    else if (builtin_info[i].parent == G_TYPE_FLAGS)
+		type_id = g_flags_register_static (builtin_info[i].type_name, (GFlagsValue *)builtin_info[i].pointer1);
+	    else if (builtin_info[i].parent == G_TYPE_BOXED )
+		type_id = g_boxed_type_register_static (builtin_info[i].type_name, (GBoxedInitFunc)builtin_info[1].pointer1, (GBoxedCopyFunc)builtin_info[i].pointer2, (GBoxedFreeFunc)builtin_info[i].pointer2, builtin_info[i].boolean1);
+	    
+	    g_assert (type_id != G_TYPE_INVALID);
+	    (*builtin_info[i].type_id) = type_id;
+	}
+    }
 }
-
