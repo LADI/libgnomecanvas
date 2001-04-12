4c9d1f928b476299da7bcdf01b5381c7dec2c735
Martin Baulig <martin@src.gnome.org>
2001-04-12 16:37:38 +0000

    Ooops, forgot to gtk_container_add() stuff.

--- demos/canvas.c
+++ demos/canvas.c
@@ -16,6 +16,8 @@ create_canvas (void)
 	notebook = gtk_notebook_new ();
 	gtk_widget_show (notebook);
 
+	gtk_container_add (GTK_CONTAINER (app), notebook);
+
 	gtk_notebook_append_page (GTK_NOTEBOOK (notebook), create_canvas_primitives (0), gtk_label_new ("Primitives"));
     	gtk_notebook_append_page (GTK_NOTEBOOK (notebook), create_canvas_primitives (1), gtk_label_new ("Antialias"));  
 	gtk_notebook_append_page (GTK_NOTEBOOK (notebook), create_canvas_arrowhead (), gtk_label_new ("Arrowhead"));
