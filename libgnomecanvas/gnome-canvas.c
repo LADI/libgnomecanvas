ec73ecbcf2ed9c9831f128553e1e63339c45405d
Joe Shaw <joe@ximian.com>
2001-05-22 01:04:48 +0000

    Emit a GDK_FOCUS_CHANGE event in the newly focused canvas item. Matches
    2001-05-21  Joe Shaw  <joe@ximian.com>
    
    	* gnome-canvas.c (gnome_canvas_item_grab_focus): Emit a
    	GDK_FOCUS_CHANGE event in the newly focused canvas item. Matches
    	behavior on the stable branch (rev 1.93.4.7).

--- libgnomecanvas/gnome-canvas.c
+++ libgnomecanvas/gnome-canvas.c
@@ -1286,6 +1286,15 @@ gnome_canvas_item_grab_focus (GnomeCanvasItem *item)
 
 	item->canvas->focused_item = item;
 	gtk_widget_grab_focus (GTK_WIDGET (item->canvas));
+
+	if (focused_item) {                                                     
+		ev.focus_change.type = GDK_FOCUS_CHANGE;                        
+		ev.focus_change.window = GTK_LAYOUT (item->canvas)->bin_window;
+		ev.focus_change.send_event = FALSE;                             
+		ev.focus_change.in = TRUE;                                      
+
+		emit_event (item->canvas, &ev);                          
+	}                               
 }
 
 
@@ -3279,7 +3288,7 @@ add_idle (GnomeCanvas *canvas)
 {
 	if (canvas->idle_id != 0)
 		return;
-
+	
 	canvas->idle_id = gtk_idle_add (idle_handler, canvas);
 }
 
