98e34296d6790282c9babbb66ce860238c0c4190
jacob berkman <jacob@ximian.com>
2001-11-07 21:40:51 +0000

    simplify by extracting our properties, then using
    2001-11-07  jacob berkman  <jacob@ximian.com>
    
    	* glade/glade-canvas.c (canvas_new): simplify by extracting our
    	properties, then using glade_standard_build_widget

--- glade/glade-canvas.c
+++ glade/glade-canvas.c
@@ -31,6 +31,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <glade/glade-init.h>
 #include <glade/glade-build.h>
 #include <libgnomecanvas/gnome-canvas.h>
@@ -49,91 +50,38 @@ canvas_new (GladeXML *xml, GType widget_type,
     int i;
     GtkWidget *wid;
 
-    GList *deferred_props = NULL, *tmp;
-    static GArray *props_array = NULL;
-    GObjectClass *oclass;
-
-    if (!props_array)
-	props_array = g_array_new(FALSE, FALSE, sizeof(GParameter));
-
-    /* we ref the class here as a slight optimisation */
-    oclass = g_type_class_ref(widget_type);
-
     for (i = 0; i < info->n_properties; i++) {
-	GParameter param = { NULL };
-	GParamSpec *pspec;
-
 	name = info->properties[i].name;
 	value = info->properties[i].value;
 
-	pspec = g_object_class_find_property(oclass, name);
+	if (!strcmp(name, "anti_aliased"))
+	    aa = (tolower (value[0] == 't') ||
+		  tolower (value[0] == 'y') ||
+		  strtol (value, NULL, 10));
 	
-	if (!pspec) {
-	    if (!strcmp(name, "anti_aliased"))
-		aa = (tolower (value[0] == 't') ||
-		      tolower (value[0] == 'y') ||
-		      atoi (value));
-	    
-	    else if (!strcmp(name, "scroll_x1"))
-		sx1 = g_strtod(value, NULL);
-	    
-	    else if (!strcmp(name, "scroll_y1"))
-		sy1 = g_strtod(value, NULL);
-	    
-	    else if (!strcmp(name, "scroll_x2"))
-		sx2 = g_strtod(value, NULL);
-	    
-	    else if (!strcmp(name, "scroll_y2"))
-		sy2 = g_strtod(value, NULL);
-	    
-	    else if (!strcmp(name, "pixels_per_unit"))
-		pixels_per_unit = g_strtod(value, NULL);
-	    else
-		g_warning("unknown property `%s' for class `%s'",
-			  name, g_type_name(widget_type));
-	    continue;
-	}
-
-	/* this should catch all properties wanting a GtkWidget
-         * subclass.  We also look for types that could hold a
-         * GtkWidget in order to catch things like the
-         * GtkAccelLabel::accel_object property.  Since we don't do
-         * any handling of GObject or GtkObject directly in
-         * glade_xml_set_value_from_string, this shouldn't be a
-         * problem. */
-	if (g_type_is_a(GTK_TYPE_WIDGET, G_PARAM_SPEC_VALUE_TYPE(pspec)) ||
-	    g_type_is_a(G_PARAM_SPEC_VALUE_TYPE(pspec), GTK_TYPE_WIDGET)) {
-	    deferred_props = g_list_prepend(deferred_props,
-					    &info->properties[i]);
-	    continue;
-	}
-
-	if (glade_xml_set_value_from_string(xml, pspec,
-					    info->properties[i].value,
-					    &param.value)) {
-	    param.name = info->properties[i].name;
-	    g_array_append_val(props_array, param);
-	}
+	else if (!strcmp(name, "scroll_x1"))
+	    sx1 = g_strtod(value, NULL);
+	
+	else if (!strcmp(name, "scroll_y1"))
+	    sy1 = g_strtod(value, NULL);
+	
+	else if (!strcmp(name, "scroll_x2"))
+	    sx2 = g_strtod(value, NULL);
+	
+	else if (!strcmp(name, "scroll_y2"))
+	    sy2 = g_strtod(value, NULL);
+	
+	else if (!strcmp(name, "pixels_per_unit"))
+	    pixels_per_unit = g_strtod(value, NULL);
     }
 
-    wid = g_object_newv(widget_type, props_array->len,
-			(GParameter *)props_array->data);
+    g_message ("ignore warnings about the following: anti_aliased, scroll_x1, scroll_y1, scroll_x2, scroll_y2, pixels_per_unit");
+
+    wid = glade_standard_build_widget (xml, widget_type, info);
 
     GNOME_CANVAS (wid)->aa = aa;
     gnome_canvas_set_scroll_region(GNOME_CANVAS(wid), sx1, sy1, sx2, sy2);
     gnome_canvas_set_pixels_per_unit(GNOME_CANVAS(wid), pixels_per_unit);
-
-    /* handle deferred properties */
-    for (tmp = deferred_props; tmp; tmp = tmp->next) {
-	GladeProperty *prop = tmp->data;
-
-	glade_xml_handle_widget_prop(xml, wid, prop->name, prop->value);
-    }
-    g_list_free(deferred_props);
-
-    g_array_set_size(props_array, 0);
-    g_type_class_unref(oclass);
-
 
     return wid;
 }
